081f55433182782ef1f326d26c29230c86e53c5c
Tim Craft <mail@timcraft.com>
2012-10-06 16:20:09 +0100

    Tweak spec descriptions/structure (less shoulding)

--- spec/nexmo_spec.rb
+++ spec/nexmo_spec.rb
@@ -9,67 +9,73 @@ describe Nexmo::Client do
   end
 
   describe 'http method' do
-    it 'should return a Net::HTTP object that uses SSL' do
+    it 'returns a net http object that uses ssl' do
       @client.http.must_be_instance_of(Net::HTTP)
       @client.http.use_ssl?.must_equal(true)
     end
   end
 
   describe 'headers method' do
-    it 'should return a hash' do
+    it 'returns a hash' do
       @client.headers.must_be_kind_of(Hash)
     end
   end
 
   describe 'send_message method' do
-    before do
-      @headers = {'Content-Type' => 'application/x-www-form-urlencoded'}
-    end
-
-    it 'should make the correct http call and return a success object if the first message status equals 0' do
-      http_response = stub(:code => '200', :body => '{"messages":[{"status":0,"message-id":"id"}]}')
+    it 'posts to the sms resource' do
+      http_response = stub(code: '200', body: '{"messages":[{"status":0,"message-id":"id"}]}')
       http_response.expects(:[]).with('Content-Type').returns('application/json;charset=utf-8')
 
       data = 'from=ruby&to=number&text=Hey%21&username=key&password=secret'
 
-      @client.http.expects(:post).with('/sms/json', data, @headers).returns(http_response)
+      headers = {'Content-Type' => 'application/x-www-form-urlencoded'}
 
-      response = @client.send_message({from: 'ruby', to: 'number', text: 'Hey!'})
+      @client.http.expects(:post).with('/sms/json', data, headers).returns(http_response)
 
-      response.success?.must_equal(true)
-      response.failure?.must_equal(false)
-      response.message_id.must_equal('id')
+      @client.send_message({from: 'ruby', to: 'number', text: 'Hey!'})
     end
 
-    it 'should return a failure object if the first message status does not equal 0' do
-      http_response = stub(:code => '200', :body => '{"messages":[{"status":2,"error-text":"Missing from param"}]}')
-      http_response.expects(:[]).with('Content-Type').returns('application/json')
+    describe 'when the first message status equals 0' do
+      it 'returns a success object' do
+        http_response = stub(code: '200', body: '{"messages":[{"status":0,"message-id":"id"}]}')
+        http_response.expects(:[]).with('Content-Type').returns('application/json;charset=utf-8')
 
-      data = 'to=number&text=Hey%21&username=key&password=secret'
+        @client.http.stubs(:post).returns(http_response)
 
-      @client.http.expects(:post).with('/sms/json', data, @headers).returns(http_response)
-
-      response = @client.send_message({to: 'number', text: 'Hey!'})
-
-      response.success?.must_equal(false)
-      response.failure?.must_equal(true)
-      response.error.to_s.must_equal('Missing from param (status=2)')
-      response.http.wont_be_nil
+        response = @client.send_message({from: 'ruby', to: 'number', text: 'Hey!'})
+        response.success?.must_equal(true)
+        response.failure?.must_equal(false)
+        response.message_id.must_equal('id')
+      end
     end
 
-    it 'should return a failure object if the server returns an unexpected http response' do
-      http_response = stub(:code => '503')
+    describe 'when the first message status does not equal 0' do
+      it 'returns a failure object' do
+        http_response = stub(code: '200', body: '{"messages":[{"status":2,"error-text":"Missing from param"}]}')
+        http_response.expects(:[]).with('Content-Type').returns('application/json')
 
-      data = 'from=ruby&to=number&text=Hey%21&username=key&password=secret'
+        @client.http.stubs(:post).returns(http_response)
 
-      @client.http.expects(:post).with('/sms/json', data, @headers).returns(http_response)
+        response = @client.send_message({to: 'number', text: 'Hey!'})
 
-      response = @client.send_message({from: 'ruby', to: 'number', text: 'Hey!'})
+        response.success?.must_equal(false)
+        response.failure?.must_equal(true)
+        response.error.to_s.must_equal('Missing from param (status=2)')
+        response.http.wont_be_nil
+      end
+    end
 
-      response.success?.must_equal(false)
-      response.failure?.must_equal(true)
-      response.error.to_s.must_equal('Unexpected HTTP response (code=503)')
-      response.http.wont_be_nil
+    describe 'when the server returns an unexpected http response' do
+      it 'returns a failure object' do
+        @client.http.stubs(:post).returns(stub(code: '503'))
+
+        response = @client.send_message({from: 'ruby', to: 'number', text: 'Hey!'})
+
+        response.success?.must_equal(false)
+        response.failure?.must_equal(true)
+        response.error.to_s.must_equal('Unexpected HTTP response (code=503)')
+        response.http.wont_be_nil
+      end
     end
   end
 
