ad82a7a44bcd9e3868c12f015d41647242ee26f6
Dominik Curyło <curylo.dominik@gmail.com>
2016-03-17 21:39:17 +0100

    Add method to Number Insight Basic API Beta

--- lib/nexmo.rb
+++ lib/nexmo.rb
@@ -118,6 +118,10 @@ module Nexmo
       post('https://api.nexmo.com/verify/control/json', params)
     end
 
+    def number_format(params)
+      post('https://api.nexmo.com/number/format/json', params)
+    end
+
     def request_number_insight(params)
       post('/ni/json', params)
     end
