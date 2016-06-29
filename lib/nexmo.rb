2d8c843fe9ec632506b694e33eb070951e509d81
Tim Craft <mail@timcraft.com>
2016-06-29 16:34:51 +0100

    Pass host as an argument to get/post helper methods

--- lib/nexmo.rb
+++ lib/nexmo.rb
@@ -26,159 +26,159 @@ module Nexmo
     end
 
     def send_message(params)
-      post("https://#@host/sms/json", params)
+      post(@host, '/sms/json', params)
     end
 
     def get_balance
-      get("https://#@host/account/get-balance")
+      get(@host, '/account/get-balance')
     end
 
     def get_country_pricing(country_code)
-      get("https://#@host/account/get-pricing/outbound", country: country_code)
+      get(@host, '/account/get-pricing/outbound', country: country_code)
     end
 
     def get_prefix_pricing(prefix)
-      get("https://#@host/account/get-prefix-pricing/outbound", prefix: prefix)
+      get(@host, '/account/get-prefix-pricing/outbound', prefix: prefix)
     end
 
     def get_sms_pricing(number)
-      get("https://#@host/account/get-phone-pricing/outbound/sms", phone: number)
+      get(@host, '/account/get-phone-pricing/outbound/sms', phone: number)
     end
 
     def get_voice_pricing(number)
-      get("https://#@host/account/get-phone-pricing/outbound/voice", phone: number)
+      get(@host, '/account/get-phone-pricing/outbound/voice', phone: number)
     end
 
     def update_settings(params)
-      post("https://#@host/account/settings", params)
+      post(@host, '/account/settings', params)
     end
 
     def topup(params)
-      post("https://#@host/account/top-up", params)
+      post(@host, '/account/top-up', params)
     end
 
     def get_account_numbers(params)
-      get("https://#@host/account/numbers", params)
+      get(@host, '/account/numbers', params)
     end
 
     def get_available_numbers(country_code, params = {})
-      get("https://#@host/number/search", {country: country_code}.merge(params))
+      get(@host, '/number/search', {country: country_code}.merge(params))
     end
 
     def buy_number(params)
-      post("https://#@host/number/buy", params)
+      post(@host, '/number/buy', params)
     end
 
     def cancel_number(params)
-      post("https://#@host/number/cancel", params)
+      post(@host, '/number/cancel', params)
     end
 
     def update_number(params)
-      post("https://#@host/number/update", params)
+      post(@host, '/number/update', params)
     end
 
     def get_message(id)
-      get("https://#@host/search/message", id: id)
+      get(@host, '/search/message', id: id)
     end
 
     def get_message_rejections(params)
-      get("https://#@host/search/rejections", params)
+      get(@host, '/search/rejections', params)
     end
 
     def search_messages(params)
-      get("https://#@host/search/messages", Hash === params ? params : {ids: Array(params)})
+      get(@host, '/search/messages', Hash === params ? params : {ids: Array(params)})
     end
 
     def send_ussd_push_message(params)
-      post("https://#@host/ussd/json", params)
+      post(@host, '/ussd/json', params)
     end
 
     def send_ussd_prompt_message(params)
-      post("https://#@host/ussd-prompt/json", params)
+      post(@host, '/ussd-prompt/json', params)
     end
 
     def send_2fa_message(params)
-      post("https://#@host/sc/us/2fa/json", params)
+      post(@host, '/sc/us/2fa/json', params)
     end
 
     def send_event_alert_message(params)
-      post("https://#@host/sc/us/alert/json", params)
+      post(@host, '/sc/us/alert/json', params)
     end
 
     def send_marketing_message(params)
-      post("https://#@host/sc/us/marketing/json", params)
+      post(@host, '/sc/us/marketing/json', params)
     end
 
     def get_event_alert_numbers
-      get("https://#@host/sc/us/alert/opt-in/query/json")
+      get(@host, '/sc/us/alert/opt-in/query/json')
     end
 
     def resubscribe_event_alert_number(params)
-      post("https://#@host/sc/us/alert/opt-in/manage/json", params)
+      post(@host, '/sc/us/alert/opt-in/manage/json', params)
     end
 
     def initiate_call(params)
-      post("https://#@host/call/json", params)
+      post(@host, '/call/json', params)
     end
 
     def initiate_tts_call(params)
-      post("https://#@api_host/tts/json", params)
+      post(@api_host, '/tts/json', params)
     end
 
     def initiate_tts_prompt_call(params)
-      post("https://#@api_host/tts-prompt/json", params)
+      post(@api_host, '/tts-prompt/json', params)
     end
 
     def send_verification_request(params)
-      post("https://#@api_host/verify/json", params)
+      post(@api_host, '/verify/json', params)
     end
 
     def check_verification_request(params)
-      post("https://#@api_host/verify/check/json", params)
+      post(@api_host, '/verify/check/json', params)
     end
 
     def get_verification_request(id)
-      get("https://#@api_host/verify/search/json", request_id: id)
+      get(@api_host, '/verify/search/json', request_id: id)
     end
 
     def control_verification_request(params)
-      post("https://#@api_host/verify/control/json", params)
+      post(@api_host, '/verify/control/json', params)
     end
 
     def get_basic_number_insight(params)
-      get("https://#@api_host/number/format/json", params)
+      get(@api_host, '/number/format/json', params)
     end
 
     def get_number_insight(params)
-      get("https://#@api_host/number/lookup/json", params)
+      get(@api_host, '/number/lookup/json', params)
     end
 
     def request_number_insight(params)
-      post("https://#@host/ni/json", params)
+      post(@host, '/ni/json', params)
     end
 
     private
 
     USER_AGENT = "ruby-nexmo/#{VERSION}/#{RUBY_VERSION}"
 
-    def get(uri, params = {})
-      uri = URI(uri)
+    def get(host, request_uri, params = {})
+      uri = URI('https://' + host + request_uri)
       uri.query = query_string(params.merge(api_key: @key, api_secret: @secret))
 
       message = Net::HTTP::Get.new(uri.request_uri)
       message['User-Agent'] = USER_AGENT
 
-      parse(request(uri, message), uri.host)
+      parse(request(uri, message), host)
     end
 
-    def post(uri, params)
-      uri = URI(uri)
+    def post(host, request_uri, params)
+      uri = URI('https://' + host + request_uri)
 
       message = Net::HTTP::Post.new(uri.request_uri)
       message.form_data = params.merge(api_key: @key, api_secret: @secret)
       message['User-Agent'] = USER_AGENT
 
-      parse(request(uri, message), uri.host)
+      parse(request(uri, message), host)
     end
 
     def request(uri, message)
