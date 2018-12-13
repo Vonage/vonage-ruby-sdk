90aa23c4f813805380c84e8622edb17b500ce595
Tim Craft <mail@timcraft.com>
2018-12-13 14:18:02 +0000

    Extract Nexmo::Keys#attribute_key method

--- lib/nexmo/entity.rb
+++ lib/nexmo/entity.rb
@@ -1,13 +1,13 @@
 module Nexmo
   class Entity
+    include Keys
+
     def initialize(**kwargs)
       @attributes = kwargs
     end
 
     def []=(key, value)
-      name = self.class.attribute_names[key]
-
-      @attributes[name] = value
+      @attributes[attribute_key(key)] = value
     end
 
     def respond_to_missing?(name, include_private = false)
@@ -31,23 +31,5 @@ module Nexmo
     attr_reader :attributes
 
     protected :attributes
-
-    private
-
-    def self.attribute_names
-      @attribute_names ||= Hash.new do |hash, key|
-        hash[key] = attribute_name(key)
-      end
-    end
-
-    def self.attribute_name(key)
-      return key if key.is_a?(Symbol)
-
-      key.split(PATTERN).join(UNDERSCORE).downcase.to_sym
-    end
-
-    PATTERN = /[\-_]|(?<=\w)(?=[A-Z])/
-
-    UNDERSCORE = '_'
   end
 end
--- lib/nexmo/keys.rb
+++ lib/nexmo/keys.rb
@@ -27,5 +27,19 @@ module Nexmo
     def camelcase_key(k)
       k.to_s.gsub(/_(\w)/) { $1.upcase }
     end
+
+    ATTRIBUTE_KEYS = Hash.new { |h, k| h[k] = k.split(PATTERN).join('_').downcase.to_sym }
+
+    PATTERN = /[\-_]|(?<=\w)(?=[A-Z])/
+
+    private_constant :ATTRIBUTE_KEYS
+
+    private_constant :PATTERN
+
+    def attribute_key(k)
+      return k if k.is_a?(Symbol)
+
+      ATTRIBUTE_KEYS[k]
+    end
   end
 end
