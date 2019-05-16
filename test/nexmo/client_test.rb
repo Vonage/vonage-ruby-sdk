Vonage/vonage-ruby-sdk

commit 673f3f31972d137fb54ca0da229d7eab726f4e0a

Add Nexmo::Error.parse method

lib/nexmo/namespace.rb
@@ -93,14 +93,8 @@ module Nexmo
         else
           response
         end
-      when Net::HTTPUnauthorized
-        raise AuthenticationError
-      when Net::HTTPClientError
-        raise ClientError
-      when Net::HTTPServerError
-        raise ServerError
       else
-        raise Error
+        Error.parse(response)
       end
     end
   end

lib/nexmo/errors/error.rb
@@ -1,4 +1,31 @@
+# frozen_string_literal: true
+require 'json'
+
 module Nexmo
   class Error < StandardError
+    def self.parse(response) # :nodoc:
+      exception_class = case response
+        when Net::HTTPUnauthorized
+          AuthenticationError
+        when Net::HTTPClientError
+          ClientError
+        when Net::HTTPServerError
+          ServerError
+        else
+          Error
+        end
+
+      message = if content_type = response['Content-Type']
+        case content_type.split(';').first
+        when 'application/problem+json'
+          Problem.parse(response.body)
+        when 'application/json'
+          hash = ::JSON.parse(response.body)
+          hash['error_title']
+        end
+      end
+
+      exception_class.new(message)
+    end
   end
 end

test/nexmo/error_test.rb
@@ -1,0 +1,69 @@
+require 'minitest/autorun'
+require 'nexmo'
+
+class NexmoErrorTest < Minitest::Test
+  def response(code)
+    Net::HTTPResponse::CODE_TO_OBJ[code.to_s].new(nil, code.to_s, nil)
+  end
+
+  def test_parse_with_401_response
+    error = Nexmo::Error.parse(response(401))
+
+    assert_kind_of Nexmo::AuthenticationError, error
+  end
+
+  def test_parse_with_4xx_response
+    error = Nexmo::Error.parse(response(400))
+
+    assert_kind_of Nexmo::ClientError, error
+  end
+
+  def test_parse_with_5xx_response
+    error = Nexmo::Error.parse(response(500))
+
+    assert_kind_of Nexmo::ServerError, error
+  end
+
+  def test_parse_with_other_response
+    error = Nexmo::Error.parse(response(101))
+
+    assert_kind_of Nexmo::Error, error
+  end
+
+  def test_parse_with_problem_response
+    problem_response = response(403).tap do |response|
+      response['Content-Type'] = 'application/problem+json'
+      response.instance_variable_set(:@read, true)
+      response.body = <<-EOS
+        {
+          "type": "https://example.com/Error#out-of-credit",
+          "title": "You do not have enough credit",
+          "detail": "Your current balance is 30, but that costs 50.",
+          "instance": "<trace_id>"
+        }
+      EOS
+    end
+
+    error = Nexmo::Error.parse(problem_response)
+
+    assert_includes error.message, 'You do not have enough credit'
+  end
+
+  def test_parse_with_invalid_parameters_response
+    invalid_parameters_response = response(400).tap do |response|
+      response['Content-Type'] = 'application/json'
+      response.instance_variable_set(:@read, true)
+      response.body = <<-EOS
+        {
+          "type": "BAD_REQUEST",
+          "error_title": "Bad Request",
+          "invalid_parameters": {"event_url": "Is required."}
+        }
+      EOS
+    end
+
+    error = Nexmo::Error.parse(invalid_parameters_response)
+
+    assert_includes error.message, 'Bad Request'
+  end
+end

commit 332612cae4c8a0c256817fbbd32928116f69e29b

Raise errors instead of returning them
Bug/regression introduced in 673f3f3

lib/nexmo/namespace.rb
@@ -94,7 +94,7 @@ module Nexmo
           response
         end
       else
-        Error.parse(response)
+        raise Error.parse(response)
       end
     end
   end

test/nexmo/client_test.rb
@@ -128,4 +128,13 @@ class NexmoClientTest < Nexmo::Test
   def test_verify_method
     assert_kind_of Nexmo::Verify, client.verify
   end
+
+  def test_raises_response_errors
+    pattern = %r{\Ahttps://rest\.nexmo\.com/}
+
+    stub_request(:get, pattern).to_return(status: 400).then.to_return(status: 500)
+
+    assert_raises(Nexmo::ClientError) { client.account.balance }
+    assert_raises(Nexmo::ServerError) { client.account.balance }
+  end
 end