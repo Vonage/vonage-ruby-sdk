9794d2192b84467cc19dd30e39c16958a786d105
Tim Craft <mail@timcraft.com>
2019-06-03 13:35:57 +0100

    Document Nexmo::NumberInsight methods

--- lib/nexmo/number_insight.rb
+++ lib/nexmo/number_insight.rb
@@ -2,18 +2,120 @@
 
 module Nexmo
   class NumberInsight < Namespace
+    # Provides basic number insight information about a number.
+    #
+    # @example
+    #   response = client.number_insight.basic(number: '447700900000')
+    #
+    # @option params [required, String] :number
+    #   A single phone number that you need insight about in national or international format.
+    #
+    # @option params [String] :country
+    #   If a number does not have a country code or is uncertain, set the two-character country code.
+    #   This code must be in ISO 3166-1 alpha-2 format and in upper case. For example, GB or US.
+    #   If you set country and number is already in E.164 format, country must match the country code in number.
+    #
+    # @param [Hash] params
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/number-insight#getNumberInsightBasic
+    #
     def basic(params)
       request('/ni/basic/json', params: params)
     end
 
+    # Provides standard number insight information about a number.
+    #
+    # @example
+    #   response = client.number_insight.standard(number: '447700900000')
+    #
+    # @option params [required, String] :number
+    #   A single phone number that you need insight about in national or international format.
+    #
+    # @option params [String] :country
+    #   If a number does not have a country code or is uncertain, set the two-character country code.
+    #   This code must be in ISO 3166-1 alpha-2 format and in upper case. For example, GB or US.
+    #   If you set country and number is already in E.164 format, country must match the country code in number.
+    #
+    # @option params [Boolean] :cnam
+    #   Indicates if the name of the person who owns the phone number should be looked up and returned in the response.
+    #   Set to true to receive phone number owner name in the response.
+    #   This features is available for US numbers only and incurs an additional charge.
+    #
+    # @param [Hash] params
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/number-insight#getNumberInsightStandard
+    #
     def standard(params)
       request('/ni/standard/json', params: params)
     end
 
+    # Provides advanced number insight information about a number synchronously.
+    #
+    # @example
+    #   response = client.number_insight.advanced(number: '447700900000')
+    #
+    # @option params [required, String] :number
+    #   A single phone number that you need insight about in national or international format.
+    #
+    # @option params [String] :country
+    #   If a number does not have a country code or is uncertain, set the two-character country code.
+    #   This code must be in ISO 3166-1 alpha-2 format and in upper case. For example, GB or US.
+    #   If you set country and number is already in E.164 format, country must match the country code in number.
+    #
+    # @option params [Boolean] :cnam
+    #   Indicates if the name of the person who owns the phone number should be looked up and returned in the response.
+    #   Set to true to receive phone number owner name in the response.
+    #   This features is available for US numbers only and incurs an additional charge.
+    #
+    # @option params [String] :ip
+    #   The IP address of the user.
+    #   If supplied, we will compare this to the country the user's phone is located in and return an error if it does not match.
+    #
+    # @param [Hash] params
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/number-insight#getNumberInsightAdvanced
+    #
     def advanced(params)
       request('/ni/advanced/json', params: params)
     end
 
+    # Provides advanced number insight number information *asynchronously* using the URL specified in the callback parameter.
+    #
+    # @example
+    #   response = client.number_insight.advanced_async(number: '447700900000', callback: webhook_url)
+    #
+    # @option params [required, String] :callback
+    #   The callback URL.
+    #
+    # @option params [required, String] :number
+    #   A single phone number that you need insight about in national or international format.
+    #
+    # @option params [String] :country
+    #   If a number does not have a country code or is uncertain, set the two-character country code.
+    #   This code must be in ISO 3166-1 alpha-2 format and in upper case. For example, GB or US.
+    #   If you set country and number is already in E.164 format, country must match the country code in number.
+    #
+    # @option params [Boolean] :cnam
+    #   Indicates if the name of the person who owns the phone number should be looked up and returned in the response.
+    #   Set to true to receive phone number owner name in the response.
+    #   This features is available for US numbers only and incurs an additional charge.
+    #
+    # @option params [String] :ip
+    #   The IP address of the user.
+    #   If supplied, we will compare this to the country the user's phone is located in and return an error if it does not match.
+    #
+    # @param [Hash] params
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/number-insight#getNumberInsightAsync
+    #
     def advanced_async(params)
       request('/ni/advanced/async/json', params: params)
     end
