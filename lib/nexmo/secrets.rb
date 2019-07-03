9a906540b1b55799084c03a67c4b82abeed1249e
Tim Craft <mail@timcraft.com>
2019-07-03 15:08:01 +0100

    Document Nexmo::Secrets methods

--- lib/nexmo/secrets.rb
+++ lib/nexmo/secrets.rb
@@ -6,18 +6,71 @@ module Nexmo
 
     self.request_body = JSON
 
+    # Create API Secret.
+    #
+    # @example
+    #   response = client.secrets.create(secret: 'T0ps3cr3t')
+    #
+    # @option params [required, String] :secret
+    #   The new secret must follow these rules:
+    #   - minimum 8 characters
+    #   - maximum 25 characters
+    #   - minimum 1 lower case character
+    #   - minimum 1 upper case character
+    #   - minimum 1 digit
+    #
+    # @param [Hash] params
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/account#createAPISecret
+    #
     def create(params)
       request('/accounts/' + account_id + '/secrets', params: params, type: Post)
     end
 
+    # Retrieve API Secrets.
+    #
+    # @example
+    #   response = client.secrets.list
+    #   response._embedded.secrets.each do |item|
+    #     puts "#{item.created_at} #{item.id}"
+    #   end
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/account#retrieveAPISecrets
+    #
     def list
       request('/accounts/' + account_id + '/secrets')
     end
 
+    # Retrieve one API Secret.
+    #
+    # @example
+    #   response = client.secrets.get(secret_id)
+    #
+    # @param [String] secret_id
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/account#retrieveAPISecret
+    #
     def get(secret_id)
       request('/accounts/' + account_id + '/secrets/' + secret_id)
     end
 
+    # Revoke an API Secret.
+    #
+    # @example
+    #   response = client.secrets.revoke(secret_id)
+    #
+    # @param [String] secret_id
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/account#revokeAPISecret
+    #
     def revoke(secret_id)
       request('/accounts/' + account_id + '/secrets/' + secret_id, type: Delete)
     end
