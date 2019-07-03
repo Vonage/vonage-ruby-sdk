2953c3eb3fec62769979b5f9ae4faf526ba8cd99
Tim Craft <mail@timcraft.com>
2019-07-03 12:28:50 +0100

    Document Nexmo::Redact methods

--- lib/nexmo/redact.rb
+++ lib/nexmo/redact.rb
@@ -6,6 +6,26 @@ module Nexmo
 
     self.request_body = JSON
 
+    # Redact a specific message.
+    #
+    # @example
+    #   response = client.redact.transaction(id: '00A0B0C0', product: 'sms')
+    #
+    # @option params [required, String] :id
+    #   The transaction ID to redact.
+    #
+    # @option params [required, String] :product
+    #   Product name that the ID provided relates to.
+    #
+    # @option params [required, String] :type
+    #   Required if redacting SMS data.
+    #
+    # @param [Hash] params
+    #
+    # @return [Entity]
+    #
+    # @see https://developer.nexmo.com/api/redact#redact-message
+    #
     def transaction(params)
       request('/v1/redact/transaction', params: params, type: Post)
     end
