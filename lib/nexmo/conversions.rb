d602ce867dd2948f5f086f9e2dc1b15cae317cf8
benhayehudi <bengreenberg@gmail.com>
2020-05-26 10:13:19 +0300

    strict typed Nexmo::Conversions

--- lib/nexmo/conversions.rb
+++ lib/nexmo/conversions.rb
@@ -1,14 +1,17 @@
-# typed: false
+# typed: strict
 # frozen_string_literal: true
 
 module Nexmo
   class Conversions < Namespace
+    extend T::Sig
     include Keys
 
+    sig { params(params: T::Hash[Symbol, T.untyped]).returns(Nexmo::Response) }
     def track_sms(params)
       request('/conversions/sms', params: hyphenate(params), type: Post)
     end
 
+    sig { params(params: T::Hash[Symbol, T.untyped]).returns(Nexmo::Response) }
     def track_voice(params)
       request('/conversions/voice', params: hyphenate(params), type: Post)
     end
