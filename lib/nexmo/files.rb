ecedf2f4224ee46bd70d752cf41a673827433827
benhayehudi <bengreenberg@gmail.com>
2020-06-03 13:10:40 +0300

    introduced strict typing to Nexmo::Files

--- lib/nexmo/files.rb
+++ lib/nexmo/files.rb
@@ -1,16 +1,20 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Nexmo
   class Files < Namespace
+    extend T::Sig
+
     self.authentication = BearerToken
 
+    sig { params(id: String).returns(T.nilable(Nexmo::Response)) }
     def get(id)
-      request('/v1/files/' + id.split('/').last)
+      request('/v1/files/' + T.must(id.split('/').last))
     end
 
+    sig { params(id: String, filename: String).returns(T.nilable(Nexmo::Response)) }
     def save(id, filename)
-      request('/v1/files/' + id.split('/').last) do |response|
+      request('/v1/files/' + T.must(id.split('/').last)) do |response|
         File.open(filename, 'wb') do |file|
           response.read_body do |chunk|
             file.write(chunk)
