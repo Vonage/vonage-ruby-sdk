8f9d0b00ff94ad555a25878beaf9a489352df7c2
benhayehudi <bengreenberg@gmail.com>
2020-06-03 13:43:06 +0300

    strict typed introduced for Nexmo::HTTP::Options

--- lib/nexmo/http.rb
+++ lib/nexmo/http.rb
@@ -1,12 +1,18 @@
-# typed: ignore
+# typed: strict
 # frozen_string_literal: true
 require 'net/http'
 
 module Nexmo
   module HTTP
     class Options
+      extend T::Sig
+
+      sig { params(hash: T::Hash[Symbol, T.untyped]).void }
       def initialize(hash)
-        @hash = hash || {}
+        raise ArgumentError, 'hash parameter cannot be empty or nil' if hash == {} || hash.nil?
+
+        @hash = T.let(@hash, T::Hash[Symbol, T.untyped]) if defined? @hash
+        @hash = hash
 
         @hash.each_key do |name|
           next if defined_options.key?(name)
@@ -15,6 +21,7 @@ module Nexmo
         end
       end
 
+      sig { params(http: Net::HTTP).returns(T::Hash[Symbol, T.untyped]) }
       def set(http)
         @hash.each do |name, value|
           http.public_send(defined_options.fetch(name), value)
@@ -23,7 +30,10 @@ module Nexmo
 
       private
 
+      sig { returns(T::Hash[Symbol, T.untyped]) }
       def defined_options
+        @defined_options = T.let(@defined_options, T.nilable(T::Hash[Symbol, T.untyped]))
+
         @defined_options ||= Net::HTTP.instance_methods.grep(/\w=\z/).each_with_object({}) do |name, hash|
           hash[name.to_s.chomp('=').to_sym] = name
         end
