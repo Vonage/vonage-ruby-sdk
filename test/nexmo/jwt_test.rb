51eef362fb7e16ff9fab492fd9bf41b18e81a2c2
benhayehudi <bengreenberg@gmail.com>
2020-07-20 17:03:28 +0300

    new specs per michael's request

--- test/nexmo/jwt_test.rb
+++ test/nexmo/jwt_test.rb
@@ -14,18 +14,20 @@ class Nexmo::JWTTest < Minitest::Test
     /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/
   end
 
+  def sample_token
+    'eyJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1OTUyNTM2MTMsImp0aSI6ImU1QmxGeDVOek5ydCIsImV4cCI6MTU5NTI1NDUxMywic3ViIjoiU3ViamVjdCIsImFwcGxpY2F0aW9uX2lkIjoieHh4eHh4eHgteHh4eC14eHh4LXh4eHgteHh4eHh4eHh4eHh4IiwidHlwIjoiSldUIn0.wQ_WHt4ba2nDWN-ju7D8VdGg6JdpVdcU2-bzvS_CMg_KVxaaD8NoYDMnIdrf4f4mOJThnmDiWvGOLFSmyqTtgN_qniCsoSL0ZnLFhvb1HWbSYsmNlOb8ePB0AkvQM-wcfHOfgToXU5tCe3OXV5s7sUDRlFEKAS4s8qzdTUAoJw5I2-RjVP9hlumV0Cda1yFyOiM1ppsV09OxbeXqKU_NLFm8XNqe_bIlB2d2XIuwtfluGXjsgPM8R2stF_hS0Q65oILue0Yru1N-IbemvLmh8Rs4hSfEKQrmabRIMOvfXdQAVTW_0g1OsGRRUsT8RAwDdcC1LB_CrktYP_o3Pp9qxA'
+  end
+
   def decode(token)
     JWT.decode(token, private_key, false, {algorithm: 'RS256'}).first
   end
 
   def test_generate_method_returns_payload_encoded_with_private_key
-    time = Time.now.to_i
+    time = Timecop.freeze(Time.now).to_i
 
     payload = {
       :application_id => application_id,
       :private_key => private_key,
-      :iat => time,
-      :exp => time + 3600,
       :jti => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
     }
 
@@ -33,7 +35,7 @@ class Nexmo::JWTTest < Minitest::Test
     decoded = decode(token)
 
     assert_equal decoded.fetch('application_id'), payload.fetch(:application_id)
-    assert_equal decoded.fetch('iat'), payload.fetch(:iat)
+    assert_equal decoded.fetch('iat'), time
     assert_equal decoded.fetch('jti'), payload.fetch(:jti)
   end
 
@@ -53,5 +55,27 @@ class Nexmo::JWTTest < Minitest::Test
     token = Nexmo::JWT.generate({ :application_id => application_id }, private_key)
 
     assert_match uuid_pattern, decode(token).fetch('jti')
+  end
+
+  def test_generate_method_sets_provided_value_for_ttl_parameter
+    payload = {
+      :application_id => application_id,
+      :private_key => private_key,
+      :ttl => 700
+    }
+
+    token = Nexmo::JWT.generate(payload)
+    decoded = decode(token)
+
+    assert_equal decoded.fetch('ttl'), payload.fetch(:ttl)
+  end
+
+  def test_generate_method_jwt_string_comparison
+    Timecop.freeze(Time.at(1595253613))
+
+    token = Nexmo::JWT.generate({ :application_id => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', :jti => 'e5BlFx5NzNrt' }, private_key)
+    decoded = decode(token)
+
+    assert_equal token, sample_token
   end
 end
