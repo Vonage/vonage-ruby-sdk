5276f7e348ba0101c3102c52ea173770f1e35efd
ohbarye <over.rye@gmail.com>
2020-10-13 01:09:07 +0900

    Add sorbet types to Vonage::GSM7

--- lib/vonage/gsm7.rb
+++ lib/vonage/gsm7.rb
@@ -1,11 +1,13 @@
-# typed: ignore
+# typed: strong
 
 module Vonage
   module GSM7
+    extend T::Sig
     CHARACTERS = "\n\f\r !\"\#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~ ¡£¤¥§¿ÄÅÆÉÑÖØÜßàäåæçèéìñòöøùüΓΔΘΛΞΠΣΦΨΩ€"
 
     REGEXP = /\A[#{Regexp.escape(CHARACTERS)}]*\z/
 
+    sig {params(string: T.nilable(String)).returns(T.nilable(Integer))}
     def self.encoded?(string)
       REGEXP =~ string
     end
