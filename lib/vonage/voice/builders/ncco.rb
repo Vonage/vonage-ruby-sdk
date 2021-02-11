d9fab2431a90217b02122ec6f6b76fb7d38fe85b
bencgreenberg <bengreenberg@gmail.com>
2021-02-11 14:55:09 +0200

    fill in remaining actions

--- lib/vonage/voice/builders/ncco.rb
+++ lib/vonage/voice/builders/ncco.rb
@@ -108,21 +108,204 @@ module Vonage
   end
 
   class Voice::Builders::Conversation
+    attr_accessor :name, :musicOnHoldUrl, :startOnEnter, :endOnExit, :record, :canSpeak, :canHear, :mute
+
+    def initialize(**attributes)
+      @name = attributes.fetch(:name)
+      @musicOnHoldUrl = attributes.fetch(:musicOnHoldUrl, nil)
+      @startOnEnter = attributes.fetch(:startOnEnter, nil)
+      @endOnExit = attributes.fetch(:endOnExit, nil)
+      @record = attributes.fetch(:record, nil)
+      @canSpeak = attributes.fetch(:canSpeak, nil)
+      @canHear = attributes.fetch(:canHear, nil)
+      @mute = attributes.fetch(:mute, nil)
+    end
+
+    def action
+      create_conversation!(self)
+    end
+
+    def create_conversation!(builder)
+      ncco = [
+        {
+          action: 'conversation',
+          name: builder.name
+        }
+      ]
+
+      ncco[0].merge!(musicOnHoldUrl: builder.musicOnHoldUrl) if builder.musicOnHoldUrl
+      ncco[0].merge!(startOnEnter: builder.startOnEnter) if builder.startOnEnter
+      ncco[0].merge!(endOnExit: builder.endOnExit) if builder.endOnExit
+      ncco[0].merge!(record: builder.record) if builder.record
+      ncco[0].merge!(canSpeak: builder.canSpeak) if builder.canSpeak
+      ncco[0].merge!(canHear: builder.canHear) if builder.canHear
+      ncco[0].merge!(mute: builder.mute) if builder.mute
+
+      ncco
+    end
   end
 
   class Voice::Builders::Input
+    attr_accessor :type, :dtmf, :speech, :eventUrl, :eventMethod
+
+    def initialize(**attributes)
+      @type = attributes.fetch(:type)
+      @dtmf = attributes.fetch(:dtmf, nil)
+      @speech = attributes.fetch(:speech, nil)
+      @eventUrl = attributes.fetch(:eventUrl, nil)
+      @eventMethod = attributes.fetch(:eventMethod, nil)
+    end
+
+    def action
+      create_input!(self)
+    end
+
+    def create_input!(builder)
+      ncco = [
+        {
+          action: 'input'
+        }
+      ]
+
+      ncco[0].merge!(dtmf: builder.dtmf) if builder.dtmf
+      ncco[0].merge!(speech: builder.speech) if builder.speech
+      ncco[0].merge!(eventUrl: builder.eventUrl) if builder.eventUrl
+      ncco[0].merge!(eventMethod: builder.eventMethod) if builder.eventMethod
+
+      ncco
+    end
   end
 
   class Voice::Builders::Notify
+    attr_accessor :payload, :eventUrl, :eventMethod
+
+    def initialize(**attributes)
+      @payload = attributes.fetch(:payload)
+      @eventUrl = attributes.fetch(:event_url)
+      @eventMethod = attributes.fetch(:event_method, nil)
+    end
+
+    def action
+      create_connect!(self)
+    end
+
+    def create_connect!(builder)
+      ncco = [
+        {
+          action: 'notify',
+          payload: builder.payload,
+          eventUrl: builder.eventUrl
+        }
+      ]
+
+      ncco[0].merge!(eventMethod: builder.eventMethod) if builder.eventMethod
+
+      ncco
+    end
   end
 
   class Voice::Builders::Record
+    attr_accessor :format, :split, :channels, :endOnSilence, :endOnKey, :timeOut, :beepStart, :eventUrl, :eventMethod
+
+    def initialize(**attributes)
+      @format = attributes.fetch(:format, nil)
+      @split = attributes.fetch(:split, nil)
+      @channels = attributes.fetch(:channels, nil)
+      @endOnSilence = attributes.fetch(:endOnSilence, nil)
+      @endOnKey = attributes.fetch(:endOnKey, nil)
+      @timeOut = attributes.fetch(:timeOut, nil)
+      @beepStart = attributes.fetch(:beepStart, nil)
+      @eventUrl = attributes.fetch(:eventUrl, nil)
+      @eventMethod = attributes.fetch(:eventMethod, nil)
+    end
+
+    def action
+      create_record!(self)
+    end
+
+    def create_record!(builder)
+      ncco = [
+        {
+          action: 'record'
+        }
+      ]
+
+      ncco[0].merge!(format: builder.format) if builder.format
+      ncco[0].merge!(split: builder.split) if builder.split
+      ncco[0].merge!(channels: builder.channels) if builder.channels
+      ncco[0].merge!(endOnSilence: builder.endOnSilence) if builder.endOnSilence
+      ncco[0].merge!(endOnKey: builder.endOnKey) if builder.endOnKey
+      ncco[0].merge!(timeOut: builder.timeOut) if builder.timeOut
+      ncco[0].merge!(beepStart: builder.beepStart) if builder.beepStart
+      ncco[0].merge!(eventUrl: builder.eventUrl) if builder.eventUrl
+      ncco[0].merge!(eventMethod: builder.eventMethod) if builder.eventMethod
+
+      ncco
+    end
   end
 
   class Voice::Builders::Stream
+    attr_accessor :streamUrl, :level, :bargeIn, :loop
+
+    def initialize(**attributes)
+      @streamUrl = attributes.fetch(:streamUrl)
+      @level = attributes.fetch(:level, nil)
+      @bargeIn = attributes.fetch(:bargeIn, nil)
+      @loop = attributes.fetch(:loop, nil)
+    end
+
+    def action
+      create_stream!(self)
+    end
+
+    def create_stream!(builder)
+      ncco = [
+        {
+          action: 'stream',
+          streamUrl: builder.streamUrl
+        }
+      ]
+
+      ncco[0].merge!(level: builder.level) if builder.level
+      ncco[0].merge!(bargeIn: builder.bargeIn) if builder.bargeIn
+      ncco[0].merge!(loop: builder.loop) if builder.loop
+
+      ncco
+    end
   end
 
   class Voice::Builders::Talk
+    attr_accessor :text, :bargeIn, :loop, :level, :language, :style
+
+    def initialize(**attributes)
+      @text = attributes.fetch(:text)
+      @bargeIn = attributes.fetch(:bargeIn, nil)
+      @loop = attributes.fetch(:loop, nil)
+      @level = attributes.fetch(:level, nil)
+      @language = attributes.fetch(:language, nil)
+      @style = attributes.fetch(:style, nil)
+    end
+
+    def action
+      create_talk!(self)
+    end
+
+    def create_talk!(builder)
+      ncco = [
+        {
+          action: 'talk',
+          text: builder.text
+        }
+      ]
+
+      ncco[0].merge!(bargeIn: builder.bargeIn) if builder.bargeIn
+      ncco[0].merge!(loop: builder.loop) if builder.loop
+      ncco[0].merge!(level: builder.level) if builder.level
+      ncco[0].merge!(language: builder.language) if builder.language
+      ncco[0].merge!(style: builder.style) if builder.style
+
+      ncco
+    end
   end
 
   class Voice::Builders::Ncco
