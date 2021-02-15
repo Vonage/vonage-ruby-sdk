252d55ce57b45e30875e329af674c4852e46d537
bencgreenberg <bengreenberg@gmail.com>
2021-02-15 12:03:10 +0200

    conversation action validations

--- lib/vonage/voice/actions/conversation.rb
+++ lib/vonage/voice/actions/conversation.rb
@@ -14,6 +14,71 @@
       @canSpeak = attributes.fetch(:canSpeak, nil)
       @canHear = attributes.fetch(:canHear, nil)
       @mute = attributes.fetch(:mute, nil)
+
+      after_initialize!
+    end
+
+    def after_initialize!
+      if self.musicOnHoldUrl
+        verify_music_on_hold_url
+      end
+
+      if self.startOnEnter
+        verify_start_on_enter
+      end
+
+      if self.endOnExit
+        verify_end_on_exit
+      end
+
+      if self.record
+        verify_record
+      end
+
+      if self.canSpeak
+        verify_can_speak
+      end
+
+      if self.canHear
+        verify_can_hear
+      end
+
+      if self.mute
+        verify_mute
+      end
+    end
+
+    def verify_music_on_hold_url
+      uri = URI.parse(self.musicOnHoldUrl)
+
+      raise ClientError.new("Invalid 'musicOnHoldUrl' value, must be a valid URI") unless uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+
+      self.musicOnHoldUrl
+    end
+
+    def verify_start_on_enter
+      raise ClientError.new("Expected 'startOnEnter' value to be a Boolean") unless self.startOnEnter == true || self.startOnEnter == false
+    end
+
+    def verify_end_on_exit
+      raise ClientError.new("Expected 'endOnExit' value to a Boolean") unless self.endOnExit == true || self.endOnExit == false
+    end
+
+    def verify_record
+      raise ClientError.new("Expected 'record' value to be a Boolean") unless self.record == true || self.record == false
+    end
+
+    def verify_can_speak
+      raise ClientError.new("Expected 'canSpeak' value to be an Array") unless self.canSpeak.is_a?(Array)
+    end
+
+    def verify_can_hear
+      raise ClientError.new("Expected 'canHear' value to be an Array") unless self.canHear.is_a?(Array)
+    end
+
+    def verify_mute
+      raise ClientError.new("Expected 'mute' value to be a Boolean") unless self.mute == true || self.mute == false
+      raise ClientError.new("The 'mute' value is not supported if the 'canSpeak' option is defined") if self.canSpeak
     end
 
     def action
