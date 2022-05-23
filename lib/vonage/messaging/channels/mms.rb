85c7ebc4e6fba35634f19c145080854d6baba356
superchilled <karl@superchilled.co.uk>
2022-05-23 15:57:22 +0100

    Defining Messages API MMS Channel class

---
+++ lib/vonage/messaging/channels/mms.rb
@@ -0,0 +1,45 @@
+# typed: true
+
+module Vonage
+  class Messaging::Channels::MMS
+    MESSAGE_TYPES = ['image', 'vcard', 'audio', 'video']
+
+    attr_reader :data
+
+    def initialize(attributes = {})
+      @type = attributes.fetch(:type, nil)
+      @message = attributes.fetch(:message, nil)
+      @opts = attributes.fetch(:opts, {})
+      @data = {}
+
+      after_initialize!
+    end
+
+    private
+
+    attr_accessor :type, :message, :opts
+    attr_writer :data
+
+    def after_initialize!
+      verify_type
+      verify_message
+      build
+    end
+
+    def build
+      data[:channel] = 'mms'
+      data[:message_type] = type
+      data[type.to_sym] = message
+      data.merge!(opts)
+    end
+
+    def verify_type
+      raise ClientError.new("Invalid message type") unless MESSAGE_TYPES.include?(type)
+    end
+
+    def verify_message
+      raise ClientError.new(":message must be a Hash") unless message.is_a? Hash
+      raise ClientError.new(":url is required in :message") unless message[:url]
+    end
+  end
+end
