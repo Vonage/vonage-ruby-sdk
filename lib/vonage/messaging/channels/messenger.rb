ad95db1418b7264079a0468f75670a784dc294db
superchilled <karl@superchilled.co.uk>
2022-05-24 12:45:55 +0100

    Defining Messages API Messenger Channel class

---
+++ lib/vonage/messaging/channels/messenger.rb
@@ -0,0 +1,50 @@
+# typed: true
+
+module Vonage
+  class Messaging::Channels::Messenger
+    MESSAGE_TYPES = ['text', 'image', 'audio', 'video', 'file']
+
+    attr_reader :data
+
+    def initialize(attributes = {})
+      @type = attributes.fetch(:type, nil)
+      @message = attributes.fetch(:message, nil)
+      @opts = attributes.fetch(:opts, {})
+      @data = {}
+
+      after_initialize!
+    end
+
+    private
+
+    attr_accessor :type, :message, :opts
+    attr_writer :data
+
+    def after_initialize!
+      verify_type
+      verify_message
+      build
+    end
+
+    def build
+      data[:channel] = 'messenger'
+      data[:message_type] = type
+      data[type.to_sym] = message
+      data.merge!(opts)
+    end
+
+    def verify_type
+      raise Vonage::ClientError.new("Invalid message type") unless MESSAGE_TYPES.include?(type)
+    end
+
+    def verify_message
+      case type
+      when 'text'
+        raise Vonage::ClientError.new(":message must be a String") unless message.is_a? String
+      else
+        raise Vonage::ClientError.new(":message must be a Hash") unless message.is_a? Hash
+        raise Vonage::ClientError.new(":url is required in :message") unless message[:url]
+      end
+    end
+  end
+end
