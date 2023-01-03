18710a7f7bd4d90846c870c505b4172aa2fddb2e
superchilled <karl@superchilled.co.uk>
2023-01-03 12:35:20 +0000

    Implementing Signature#secure_compare method

--- lib/vonage/signature.rb
+++ lib/vonage/signature.rb
@@ -32,7 +32,7 @@ module Vonage
 
       signature = params.delete('sig')
 
-      ::JWT::SecurityUtils.secure_compare(signature, digest(params, signature_method))
+      secure_compare(signature, digest(params, signature_method))
     end
 
     private
@@ -48,6 +48,17 @@ module Vonage
       else
         raise ArgumentError, "Unknown signature algorithm: #{signature_method}. Expected: md5hash, md5, sha1, sha256, or sha512."
       end
+    end
+
+    def secure_compare(left, right)
+      left_bytesize = left.bytesize
+
+      return false unless left_bytesize == right.bytesize
+
+      unpacked_left = left.unpack "C#{left_bytesize}"
+      result = 0
+      right.each_byte { |byte| result |= byte ^ unpacked_left.shift }
+      result.zero?
     end
   end
 end
