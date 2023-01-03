99c49151f52fa46180ff7fec99becc9ed244de86
superchilled <karl@superchilled.co.uk>
2023-01-03 12:56:58 +0000

    Removing own implementation of secure_compare and instead updating namespacing for SecurityUtils module

--- lib/vonage/signature.rb
+++ lib/vonage/signature.rb
@@ -32,7 +32,7 @@ module Vonage
 
       signature = params.delete('sig')
 
-      secure_compare(signature, digest(params, signature_method))
+      ::JWT::Algos::Hmac::SecurityUtils.secure_compare(signature, digest(params, signature_method))
     end
 
     private
@@ -48,17 +48,6 @@ module Vonage
       else
         raise ArgumentError, "Unknown signature algorithm: #{signature_method}. Expected: md5hash, md5, sha1, sha256, or sha512."
       end
-    end
-
-    def secure_compare(left, right)
-      left_bytesize = left.bytesize
-
-      return false unless left_bytesize == right.bytesize
-
-      unpacked_left = left.unpack "C#{left_bytesize}"
-      result = 0
-      right.each_byte { |byte| result |= byte ^ unpacked_left.shift }
-      result.zero?
     end
   end
 end
