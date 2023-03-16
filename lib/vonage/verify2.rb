7529cf4ce752bd3ee43f3bba24917b07a2a56339
superchilled <karl@superchilled.co.uk>
2023-03-16 18:46:07 +0000

    Adding initial Verify2 tests

--- lib/vonage/verify2.rb
+++ lib/vonage/verify2.rb
@@ -7,13 +7,17 @@ module Vonage
 
     self.request_body = JSON
 
-    # Request a verification be sent to a user .
+    # Request a verification be sent to a user.
     #
     # @see https://developer.vonage.com/en/api/verify.v2#newRequest
     #
     def start_verfication(workflow:, **opts)
     end
 
+    # Check a supplied code against a request to see if it is valid.
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#checkCode
+    #
     def check_code(request_id:, code:)
     end
   end
---
+++ test/vonage/verify2_test.rb
@@ -0,0 +1,96 @@
+# typed: false
+require_relative './test'
+
+class Vonage::Verify2Test < Vonage::Test
+  def verify2
+    Vonage::Verify2.new(config)
+  end
+
+  def request_id
+    'c11236f4-00bf-4b89-84ba-88b25df97315'
+  end
+
+  def response
+    {
+      headers: response_headers,
+      body: '{"request_id":"c11236f4-00bf-4b89-84ba-88b25df97315"}'
+    }
+  end
+
+  def uri
+    'https://api.nexmo.com/v2/verify/'
+  end
+
+  def check_request_uri
+    uri + request_id
+  end
+
+  def to_number
+    '447700900000'
+  end
+
+  def test_start_verfication_method
+    workflow = [{channel: 'sms', to: to_number}]
+
+    stub_request(:post, uri).with(headers: headers, body: {workflow: workflow}).to_return(response)
+
+    assert_kind_of Vonage::Response, verify2.start_verfication(workflow: workflow)
+  end
+
+  def test_start_verfication_method_with_opts
+    workflow = [{channel: 'sms', to: to_number}]
+    params = {workflow: workflow, brand: 'Example Brand'}
+
+    stub_request(:post, uri).with(headers: headers, body: params).to_return(response)
+
+    assert_kind_of Vonage::Response, verify2.start_verfication(workflow: workflow, brand: 'Example Brand')
+  end
+
+  def test_start_verfication_method_without_workflow
+    assert_raises ArgumentError do
+      verify.start_verfication
+    end
+  end
+
+  def test_start_verfication_method_with_workflow_that_isnt_array
+    workflow = {channel: 'sms', to: to_number}
+
+    assert_raises ArgumentError do
+      verify.start_verfication(workflow: workflow)
+    end
+  end
+
+  def test_start_verfication_method_with_empty_workflow
+    assert_raises ArgumentError do
+      verify.start_verfication(workflow: [])
+    end
+  end
+
+  def test_start_verfication_method_with_multiple_workflows
+    workflow = [{channel: 'sms', to: to_number}, {channel: 'voice', to: to_number}]
+
+    stub_request(:post, uri).with(headers: headers, body: {workflow: workflow}).to_return(response)
+
+    assert_kind_of Vonage::Response, verify2.start_verfication(workflow: workflow)
+  end
+
+  def test_check_code_method
+    code = '1234'
+
+    stub_request(:post, check_request_uri).with(headers: headers, body: {code: code}).to_return(status: 200)
+
+    assert_kind_of Vonage::Response, verify2.check_code(request_id: request_id, code: code)
+  end
+
+  def test_check_code_method_without_request_id
+    assert_raises ArgumentError do
+      verify.check_code(code: '1234')
+    end
+  end
+
+  def test_check_code_method_without_code
+    assert_raises ArgumentError do
+      verify.check_code(request_id: request_id)
+    end
+  end
+end
