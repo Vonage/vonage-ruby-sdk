d6c26735069a1c39caf44f642288fb174459d8ca
superchilled <karl@superchilled.co.uk>
2023-05-02 10:17:45 +0100

    Implementing List methods

--- lib/vonage/proactive_connect/list.rb
+++ lib/vonage/proactive_connect/list.rb
@@ -7,8 +7,49 @@ module Vonage
 
     self.authentication = BearerToken
 
+    self.host = :vonage_host
+
     self.request_body = JSON
 
+    def create(name:, **params)
+      request(
+        "/v0.1/bulk/lists",
+        params: params.merge({ name: name }),
+        type: Post
+      )
+    end
 
+    def find(id:)
+      request("/v0.1/bulk/lists/#{id}")
+    end
+
+    def update(id:, name:, **params)
+      request(
+        "/v0.1/bulk/lists/#{id}",
+        params: params.merge({ name: name }),
+        type: Put
+      )
+    end
+
+    def delete(id:)
+      request(
+        "/v0.1/bulk/lists/#{id}",
+        type: Delete
+      )
+    end
+
+    def clear_items(id:)
+      request(
+        "/v0.1/bulk/lists/#{id}/clear",
+        type: Post
+      )
+    end
+
+    def fetch_and_replace_items(id:)
+      request(
+        "/v0.1/bulk/lists/#{id}/fetch",
+        type: Post
+      )
+    end
   end
 end
--- test/vonage/proactive_connect/list_test.rb
+++ test/vonage/proactive_connect/list_test.rb
@@ -22,7 +22,6 @@ class Vonage::ProactiveConnect::ListTest < Vonage::Test
   end
 
   def test_create_method_with_optional_params
-    skip
     stub_request(:post, list_uri).with(
       request(body: { name: "Test List", description: "foo", tags: ["bar", "baz"]})
     ).to_return(response)
@@ -31,83 +30,70 @@ class Vonage::ProactiveConnect::ListTest < Vonage::Test
   end
 
   def test_create_method_without_name
-    skip
     assert_raises(ArgumentError) { list.create }
   end
 
   def test_find_method
-    skip
     stub_request(:get, list_uri + "/#{list_id}").to_return(response)
 
     assert_kind_of Vonage::Response, list.find(id: list_id)
   end
 
   def test_find_method_without_id
-    skip
     assert_raises(ArgumentError) { list.find }
   end
 
   def test_update_method
-    skip
     stub_request(:put, list_uri + "/#{list_id}").with(
       request(body: { name: "Test List Updated" })
     ).to_return(response)
 
-    assert_kind_of Vonage::Response, list.create(id: list_id, name: "Test List Updated")
+    assert_kind_of Vonage::Response, list.update(id: list_id, name: "Test List Updated")
   end
 
   def test_update_method_with_optional_params
-    skip
     stub_request(:put, list_uri + "/#{list_id}").with(
       request(body: { name: "Test List Updated", description: "qux", tags: ["quux", "xyzzy"]})
     ).to_return(response)
 
-    assert_kind_of Vonage::Response, list.create(id: list_id, name: "Test List Updated", description: "qux", tags: ["quux", "xyzzy"])
+    assert_kind_of Vonage::Response, list.update(id: list_id, name: "Test List Updated", description: "qux", tags: ["quux", "xyzzy"])
   end
 
   def test_update_method_without_id
-    skip
-    assert_raises(ArgumentError) { list.create(name: "Test List Updated") }
+    assert_raises(ArgumentError) { list.update(name: "Test List Updated") }
   end
 
   def test_update_method_without_name
-    skip
-    assert_raises(ArgumentError) { list.create(id: list_id) }
+    assert_raises(ArgumentError) { list.update(id: list_id) }
   end
 
   def test_delete_method
-    skip
     stub_request(:delete, list_uri + "/#{list_id}").to_return(response)
 
     assert_kind_of Vonage::Response, list.delete(id: list_id)
   end
 
   def test_delete_method_without_id
-    skip
     assert_raises(ArgumentError) { list.delete }
   end
 
   def test_clear_items_method
-    skip
     stub_request(:post, list_uri + "/#{list_id}/clear").to_return(response)
 
     assert_kind_of Vonage::Response, list.clear_items(id: list_id)
   end
 
   def test_clear_items_method_without_id
-    skip
     assert_raises(ArgumentError) { list.clear_items }
   end
 
   def test_fetch_and_replace_items_method
-    skip
     stub_request(:post, list_uri + "/#{list_id}/fetch").to_return(response)
 
-    assert_kind_of Vonage::Response, list.fetch_and_replace(id: list_id)
+    assert_kind_of Vonage::Response, list.fetch_and_replace_items(id: list_id)
   end
 
   def test_fetch_and_replace_items_method_without_id
-    skip
-    assert_raises(ArgumentError) { list.fetch_and_replace }
+    assert_raises(ArgumentError) { list.fetch_and_replace_items }
   end
 end
