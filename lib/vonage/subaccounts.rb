ac41662bc11982e035f2c8152ed2ae8190feca7e
superchilled <karl@superchilled.co.uk>
2023-06-14 11:44:15 +0100

    Creating file structure and stubbing out methods

---
+++ lib/vonage/subaccounts.rb
@@ -0,0 +1,45 @@
+# typed: strict
+# frozen_string_literal: true
+
+module Vonage
+  class Account < Namespace
+
+    self.host = :rest_host
+
+    def list
+
+    end
+
+    def find
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def list_credit_transfers
+
+    end
+
+    def transfer_credit
+
+    end
+
+    def list_balance_transfers
+
+    end
+
+    def transfer_balance
+
+    end
+
+    def transfer_number
+
+    end
+  end
+end
