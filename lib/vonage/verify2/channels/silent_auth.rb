df2a619c06d1b2907c88593361af47998a097eed
superchilled <karl@superchilled.co.uk>
2023-12-03 13:55:23 +0000

    Updating silent auth workflow options

--- lib/vonage/verify2/channels/silent_auth.rb
+++ lib/vonage/verify2/channels/silent_auth.rb
@@ -5,16 +5,24 @@ require 'phonelib'
 module Vonage
   class Verify2::Channels::SilentAuth
 
-    attr_reader :channel, :to
+    attr_reader :channel, :to, :sandbox
+    attr_accessor :redirect_url
 
-    def initialize(to:)
+    def initialize(to:, redirect_url: nil, sandbox: false)
       self.channel = 'silent_auth'
       self.to = to
+      self.redirect_url = redirect_url if redirect_url
+      self.sandbox = sandbox
     end
 
     def to=(to)
       raise ArgumentError, "Invalid 'to' value #{to}. Expected to be in E.164 format" unless Phonelib.parse(to.to_i).valid?
       @to = to
+    end
+
+    def sandbox=(sandbox)
+      raise ArgumentError, "Invalid 'sandbox' value #{sandbox}. Expected to be boolean value" unless [true, false].include? sandbox
+      @sandbox = sandbox
     end
 
     def to_h
