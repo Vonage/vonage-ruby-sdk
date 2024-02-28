787c0596e0e96fb6aa8f69046f4193ff24dba930
superchilled <karl@superchilled.co.uk>
2024-02-28 16:26:34 +0000

    Implementing Conversation API

--- lib/vonage/conversation.rb
+++ lib/vonage/conversation.rb
@@ -8,5 +8,43 @@ module Vonage
     self.authentication = BearerToken
 
     self.request_body = JSON
+
+    def list(**params)
+      request('/v1/conversations', params: params, response_class: ListResponse)
+    end
+
+    def create(**params)
+      request('/v1/conversations', params: params, type: Post)
+    end
+
+    def find(conversation_id:)
+      request("/v1/conversations/#{conversation_id}")
+    end
+
+    def update(conversation_id:, **params)
+      request("/v1/conversations/#{conversation_id}", params: params, type: Put)
+    end
+
+    def delete(conversation_id:)
+      request("/v1/conversations/#{conversation_id}", type: Delete)
+    end
+
+    # @return [User]
+    sig { returns(T.nilable(Vonage::Conversation::User)) }
+    def user
+      @user ||= User.new(@config)
+    end
+
+    # @return [Member]
+    sig { returns(T.nilable(Vonage::Conversation::Member)) }
+    def member
+      @member ||= Member.new(@config)
+    end
+
+    # @return [Event]
+    sig { returns(T.nilable(Vonage::Conversation::Event)) }
+    def event
+      @event ||= Event.new(@config)
+    end
   end
 end
