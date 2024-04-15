04a0d58e95a500ac46f87639898c7ae9199d8112
superchilled <karl@superchilled.co.uk>
2024-04-15 16:59:20 +0100

    Adding Renders implementation

--- lib/vonage/video/renders.rb
+++ lib/vonage/video/renders.rb
@@ -3,12 +3,34 @@
 
 module Vonage
   class Video::Renders < Namespace
+    include Keys
+
     self.authentication = BearerToken
 
     self.request_body = JSON
 
     self.host = :video_host
 
-    
+    def start(session_id:, token:, url:, **params)
+      request(
+        '/v2/project/' + @config.application_id + '/render',
+        params: camelcase(params.merge({sessionId: session_id, token: token, url: url})),
+        type: Post)
+    end
+
+    def stop(experience_composer_id:)
+      request('/v2/project/' + @config.application_id + '/render/' + experience_composer_id, type: Delete)
+    end
+
+    def info(experience_composer_id:)
+      request('/v2/project/' + @config.application_id + '/render/' + experience_composer_id)
+    end
+
+    def list(**params)
+      path = '/v2/project/' + @config.application_id + '/render'
+      path += "?#{Params.encode(camelcase(params))}" unless params.empty?
+
+      request(path, response_class: ListResponse)
+    end
   end
 end
