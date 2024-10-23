cea815871e12e22349c08e383213fdd7d724b3d3
superchilled <karl@superchilled.co.uk>
2024-10-23 12:00:47 +0100

    Adding code comments for templates and template fragments

--- lib/vonage/verify2/template_fragments.rb
+++ lib/vonage/verify2/template_fragments.rb
@@ -9,14 +9,65 @@ module Vonage
 
     self.request_body = JSON
 
+    # Get a list of of template fragments for a specific template.
+    #
+    # @example
+    #   template_fragment_list = client.verify2.template_fragments.list(template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9')
+    #
+    # @param [required, String] :template_id. The ID of the template for which to retreive the fragments
+    #
+    # @param [optional, Integer] :page_size. The amount of template fragments to list per page
+    #
+    # @param [optional, Integer] :page. The page number to retrieve
+    #
+    # @return [ListResponse]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#listTemplateFragments
     def list(template_id:, **params)
       request("/v2/verify/templates/#{template_id}/template_fragments", params: params, response_class: ListResponse)
     end
 
+    # Get details of a specific template fragment.
+    #
+    # @example
+    #   template_fragment = client.verify2.template_fragments.info(
+    #                         template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9',
+    #                         template_fragment_id: 'c70f446e-997a-4313-a081-60a02a31dc19'
+    #                       )
+    #
+    # @param [required, String] :template_id. The ID of the template for which to retreive the fragment
+    #
+    # @param [required, String] :template_fragment_id. The ID of the fragment to be retreived
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#getTemplateFragment
     def info(template_id:, template_fragment_id:)
       request("/v2/verify/templates/#{template_id}/template_fragments/#{template_fragment_id}")
     end
 
+    # Create a new template fragment.
+    #
+    # @example
+    #   client.verify2.template_fragments.create(
+    #                         template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9',
+    #                         channel: 'sms',
+    #                         locale: 'en-gb',
+    #                         text: 'Your code is: ${code}'
+    #                       )
+    #
+    # @param [required, String] :template_id. The ID of the template for which to create the fragment
+    #
+    # @param [required, String] :channel. The verification channel for which to create the fragment. Must be one of 'sms' or 'voice'
+    #
+    # @param [required, String] :locale. The locale for which to create the fragment.
+    #
+    # @param [required, String] :text. The text to be used in the template fragment.
+    #   There are 4 reserved variables available to use as part of the text: ${code}, ${brand}, ${time-limit} and ${time-limit-unit}
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#addTemplateFragmentToTemplate
     def create(template_id:, channel:, locale:, text:)
       raise ArgumentError, "Invalid 'channel' #{channel}. Must be one of #{CHANNELS.join(', ')}" unless CHANNELS.include?(channel)
       request(
@@ -29,10 +80,44 @@ module Vonage
         type: Post)
     end
 
+    # Update an existing template fragment.
+    #
+    # @example
+    #   client.verify2.template_fragments.update(
+    #                         template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9',
+    #                         template_fragment_id: 'c70f446e-997a-4313-a081-60a02a31dc19,
+    #                         text: 'Your one-time code is: ${code}'
+    #                       )
+    #
+    # @param [required, String] :template_id. The ID of the template with which the fragment to be updated is associated
+    #
+    # @param [required, String] :template_fragment_id. The ID of the fragment to be updated
+    #
+    # @param [required, String] :text. The text to be used in the template fragment.
+    #   There are 4 reserved variables available to use as part of the text: ${code}, ${brand}, ${time-limit} and ${time-limit-unit}
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#updateTemplateFragment
     def update(template_id:, template_fragment_id:, text:)
       request("/v2/verify/templates/#{template_id}/template_fragments/#{template_fragment_id}", params: {text: text}, type: Patch)
     end
 
+    # Delete a template fragment.
+    #
+    # @example
+    #   client.verify2.template_fragments.delete(
+    #                         template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9',
+    #                         template_fragment_id: 'c70f446e-997a-4313-a081-60a02a31dc19'
+    #                       )
+    #
+    # @param [required, String] :template_id. The ID of the template with which the fragment to be deleted is associated
+    #
+    # @param [required, String] :template_fragment_id. The ID of the fragment to be deleted
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#deleteTemplateFragment
     def delete(template_id:, template_fragment_id:)
       request("/v2/verify/templates/#{template_id}/template_fragments/#{template_fragment_id}", type: Delete)
     end
--- lib/vonage/verify2/templates.rb
+++ lib/vonage/verify2/templates.rb
@@ -7,22 +7,78 @@ module Vonage
 
     self.request_body = JSON
 
+    # Get a list of all templates.
+    #
+    # @example
+    #   template_list = client.verify2.templates.list
+    #
+    # @param [optional, Integer] :page_size. The amount of templates to list per page
+    #
+    # @param [optional, Integer] :page. The page number to retrieve
+    #
+    # @return [ListResponse]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#listTemplates
     def list(**params)
       request('/v2/verify/templates', params: params, response_class: ListResponse)
     end
 
+    # Get details of a specific template.
+    #
+    # @example
+    #   template = client.verify2.templates.info(template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9')
+    #
+    # @param [required, String] :template_id. The ID of the template to be retreived
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#getTemplate
     def info(template_id:)
       request('/v2/verify/templates/' + template_id)
     end
 
+    # Create a new template.
+    #
+    # @example
+    #   client.verify2.templates.create(name: 'my-template')
+    #
+    # @param [required, String] :name. The name of the template. The following characters are permitted: [A-Z a-z 0-9 _ -]
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#createTemplate
     def create(name:)
       request('/v2/verify/templates', params: { name: name }, type: Post)
     end
 
+    # Update an existing template.
+    #
+    # @example
+    #   client.verify2.templates.update(template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9', name: 'my-updated-template')
+    #
+    # @param [required, String] :template_id. The ID of the template to be updated
+    #
+    # @param [optional, String] :name. The name of the template. The following characters are permitted: [A-Z a-z 0-9 _ -]
+    #
+    # @param [optional, Boolean] :is_default. Whether the template is the default template for a specific locale/channel combination
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#updateTemplate
     def update(template_id:, **params)
       request('/v2/verify/templates/' + template_id, params: params, type: Patch)
     end
 
+    # Delete a template.
+    #
+    # @example
+    #   client.verify2.templates.delete(template_id: '8f35a1a7-eb2f-4552-8fdf-fffdaee41bc9')
+    #
+    # @param [required, String] :template_id. The ID of the template to be deleted
+    #
+    # @return [Response]
+    #
+    # @see https://developer.vonage.com/en/api/verify.v2#deleteTemplate
     def delete(template_id:)
       request('/v2/verify/templates/' + template_id, type: Delete)
     end
