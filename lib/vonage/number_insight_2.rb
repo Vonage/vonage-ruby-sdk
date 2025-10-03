a9e38a390293253d981758fd568c37e0d20080c1
superchilled <karl@superchilled.co.uk>
2025-10-03 15:28:22 +0100

    Deprecating NI v2

--- lib/vonage/number_insight_2.rb
+++ lib/vonage/number_insight_2.rb
@@ -9,25 +9,10 @@ module Vonage
 
     self.request_body = JSON
 
-    # Make fraud check requests with a phone number by looking up fraud score and/or by checking sim swap status.
-    #
-    # @example
-    #   response = client.number_insight_2.fraud_check(type: 'phone', phone: '447900000000', insights: ['fraud_score'])
-    #
-    # @param [required, String] :type The type of number to check.
-    #   Accepted value is “phone” when a phone number is provided.
-    #
-    # @param [required, String] :phone A single phone number that you need insight about in the E.164 format.
-    #
-    # @param [required, Array] :insights An array of strings indicating the fraud check insights required for the number.
-    #   Must be least one of: `fraud_score`, `sim_swap`
-    #
-    # @return [Response]
-    #
-    # @see https://developer.vonage.com/en/api/number-insight.v2#fraud_check
-    #
+   # @deprecated
     sig { params(type: String, phone: String, insights: T::Array[String]).returns(Vonage::Response) }
     def fraud_check(type:, phone:, insights:)
+      logger.info('This method is deprecated and will be removed in a future release.')
       raise ArgumentError.new("`insights` must not be an empty") if insights.empty?
 
       request('/v2/ni', params: {type: type, phone: phone, insights: insights}, type: Post)
