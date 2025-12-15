b7e52f82f2547f9a25529cd2eba2db01713a07c7
superchilled <karl@superchilled.co.uk>
2025-12-15 12:28:52 +0000

    Adding generate method for Signature class

--- lib/vonage/signature.rb
+++ lib/vonage/signature.rb
@@ -10,6 +10,28 @@ module Vonage
       @config = config
     end
 
+    # Generate a request signature.
+    #
+    # @example
+    #   client = Vonage::Client.new
+    #   client.config.signature_secret = 'secret'
+    #   client.config.signature_method = 'sha512'
+    #   params = {
+    #     'to' => '447900000000',
+    #     'from' => '447900000001',
+    #     'text' => 'Hello World',
+    #     'timestamp' => '1385047698'
+    #   }
+    #
+    #   sig = client.signature.generate(params)
+    #
+    # @param [Hash] params
+    #
+    # @see https://developer.nexmo.com/concepts/guides/signing-messages
+    def generate(params, signature_secret: @config.signature_secret, signature_method: @config.signature_method)
+      digest(params, signature_secret, signature_method)
+    end
+    
     # Check webhook request signature.
     #
     # @example
