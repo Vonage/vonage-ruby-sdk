2ab6f25c05651591a9eae7b9b21cbe2c7adb457e
superchilled <karl@superchilled.co.uk>
2026-01-28 17:48:20 +0000

    Implementing Identity INsights requests method

--- lib/vonage/identity_insights.rb
+++ lib/vonage/identity_insights.rb
@@ -3,5 +3,30 @@
 
 module Vonage
   class IdentityInsights < Namespace
+    VALID_INSIGHT_TYPES = %i[format sim_swap original_carrier current_carrier].freeze
+    
+    self.authentication = BearerToken
+
+    self.host = :vonage_host
+
+    self.request_body = JSON
+
+    # Submit an Identity Insights request.
+    def requests(phone_number:, insights: {}, **options)
+
+      raise ArgumentError.new("`phone_number` must be in E.164 format") unless Phonelib.parse(phone_number).valid?
+      raise ArgumentError.new("`insights` must be a Hash or instance of InsightsBuilder") unless insights.is_a?(Hash)
+      raise ArgumentError.new("`insights` cannot be empty") if insights.to_h.empty?
+
+      invalid_insights = insights.keys - VALID_INSIGHT_TYPES
+      raise ArgumentError.new("`insights` contains the following invalid or unsupported types: #{invalid_insights.join(', ')}") unless invalid_insights.empty?
+
+      params = {
+        phone_number: phone_number,
+        insights: insights
+      }.merge(options)
+
+      request('/identity-insights/v1/requests', params: params, type: Post)
+    end
   end
 end
---
+++ test/vonage/identity_insights_test.rb
@@ -0,0 +1,110 @@
+# typed: false
+require_relative './test'
+
+class Vonage::IdentityInsightsTest < Vonage::Test
+  def identity_insights
+    Vonage::IdentityInsights.new(config)
+  end
+
+  def geospecific_identity_insights
+    config = Vonage::Config.new.merge(
+      {
+        application_id: application_id,
+        private_key: private_key,
+        vonage_host: 'api-us.vonage.com'
+      }
+    )
+
+    Vonage::IdentityInsights.new(config)
+  end
+
+  def default_host
+    'https://api-eu.vonage.com'
+  end
+
+  def geospecific_host
+    'https://api-us.vonage.com'
+  end
+
+  def product_path
+    '/identity-insights/v1/requests'
+  end
+
+  def identity_insights_uri
+    default_host + product_path
+  end
+
+  def geospecific_identity_insights_uri
+    geospecific_host + product_path
+  end
+
+  def phone_number
+    '447900000000'
+  end
+
+  def test_requests_method
+    params = {
+      phone_number: phone_number,
+      insights: {
+        format: {}
+      }
+    }
+
+    stub_request(:post, identity_insights_uri).with(request(body: params)).to_return(response)
+
+    assert_kind_of Vonage::Response, identity_insights.requests(phone_number: phone_number, insights: { format: {} })
+  end
+
+  def test_requests_method_with_optional_parameters
+    params = {
+      phone_number: phone_number,
+      purpose: 'FraudPreventionAndDetection',
+      insights: {
+        sim_swap: {}
+      }
+    }
+
+    stub_request(:post, identity_insights_uri).with(request(body: params)).to_return(response)
+
+    response = identity_insights.requests(
+      phone_number: phone_number,
+      purpose: 'FraudPreventionAndDetection',
+      insights: { sim_swap: {} }
+    )
+
+    assert_kind_of Vonage::Response, response
+  end
+
+  def test_requests_method_with_geospecific_host
+    params = {
+      phone_number: phone_number,
+      insights: {
+        format: {}
+      }
+    }
+
+    stub_request(:post, geospecific_identity_insights_uri).with(request(body: params)).to_return(response)
+
+    assert_kind_of Vonage::Response, geospecific_identity_insights.requests(phone_number: phone_number, insights: { format: {} })
+  end
+
+  def test_requests_method_without_phone_number
+    assert_raises(ArgumentError) { identity_insights.requests(insights: { format: {} }) }
+  end
+
+  def test_requests_method_with_invalid_phone_number
+    assert_raises(ArgumentError) { identity_insights.requests(phone_number: '0000000000000', insights: { format: {} }) }
+  end
+
+  def test_requests_method_with_empty_insights
+    assert_raises(ArgumentError) { identity_insights.requests(phone_number: phone_number) }
+  end
+
+  def test_requests_method_with_invalid_insights_class
+    assert_raises(ArgumentError) { identity_insights.requests(phone_number: phone_number, insights: ['format']) }
+  end
+
+  def test_requests_method_with_invalid_insights_type
+    assert_raises(ArgumentError) { identity_insights.requests(phone_number: phone_number, insights: { foo: {} }) }
+  end
+end
