659454a49032e19f51a302b404bcbc11474310b6
superchilled <karl@superchilled.co.uk>
2026-01-29 16:02:09 +0000

    Adding YARD doc code comments for IdentityInsights public methods

--- lib/vonage/identity_insights.rb
+++ lib/vonage/identity_insights.rb
@@ -12,6 +12,37 @@ module Vonage
     self.request_body = JSON
 
     # Submit an Identity Insights request.
+    #  The SDK currently supports the following insight types: `format`, `sim_swap`, `current_carrier`, and `previous_carrier`.
+    #
+    # @example with `insights` as a Hash
+    #   response = client.identity_insights.requests(phone_number: '447900000000', insights: { format: {} })
+    #
+    # @example with `insights` as an InsightsBuilder
+    #   insights = client.identity_insights.insights_builder.add_format.add_sim_swap(period: 180)
+    #   response = client.identity_insights.requests(phone_number: '447900000000', insights: insights)
+    #
+    # @example with a block to build `insights`
+    #   response = client.identity_insights.requests(phone_number: '447900000000') do |builder|
+    #     builder.add_format
+    #     builder.add_sim_swap(period: 180)
+    #   end
+    #
+    # @option params [required, String] :phone_number The phone number to request insights for, E.164 formatted.
+    #
+    # @option params [String] :purpose The purpose for requesting the insights.
+    #
+    # @option params [Hash, InsightsBuilder] :insights
+    #   A Hash or InsightsBuilder instance specifying the types of insights to request.
+    #   See the Vonage developer documentation for any parameters supported by each insight type.
+    #
+    # for block {|builder| ... }
+    # @yield [builder] passes a InsightsBuilder instance to the block
+    # @yieldreturn [required, InsightsBuilder] expects the block to return a InsightsBuilder instance
+    #
+    # @return [Vonage::Response] The API response.
+    #
+    # @see https://developer.vonage.com/en/api/identity-insights#getInsights
+    #
     def requests(phone_number:, insights: {}, **options)
       insights = yield insights_builder if block_given?
 
@@ -26,6 +57,9 @@ module Vonage
       request('/identity-insights/v1/requests', params: params, type: Post)
     end
 
+    # Instantiate an InsightsBuilder.
+    # @return [InsightsBuilder] A new InsightsBuilder instance.
+    #
     def insights_builder
       InsightsBuilder.new
     end
